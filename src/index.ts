848ade2222bf8f60e6bbc3cccaed26c5d84d0285
陨落基围虾 <3161880837@qq.com>
2026-03-08 14:37:50 +0800

    fix: 导出ObtainVMMethod类型以解决外部使用问题

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as obtainVM from "./vm";
 
-type ObtainVMMethod = keyof typeof obtainVM extends `obtainBy${infer R extends string}` ? R : never;
+export type ObtainVMMethod = keyof typeof obtainVM extends `obtainBy${infer R extends string}` ? R : never;
 export async function vm(priority: ObtainVMMethod[]) {
     for (const method of priority) {
         const vm = await obtainVM[`obtainBy${method}`]();
